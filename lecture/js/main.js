cd34d6ea7220167dc40deb1c3ca76f8cd26631fe
kk3june <kk3june@gmail.com>
2021-03-08 13:46:44 +0900

    lecture  add random image complete

---
+++ lecture/js/main.js
@@ -0,0 +1,34 @@
+const field = document.querySelector('.game__field');
+const fieldRect = field.getBoundingClientRect();
+
+function initGame() {
+    console.log(fieldRect);
+    addItem('carrot', 5, '/lecture/img/carrot.png');
+    addItem('bug', 5, '/lecture/img/bug.png');
+}
+
+function addItem(className, count, imgPath) {
+    const x1 = 0;
+    const y1 = 0;
+    const x2 = fieldRect.width;
+    const y2 = fieldRect.height;
+
+    for (let i = 0; i < count; i++) {
+        const item = document.createElement('img');
+        item.setAttribute('class', className);
+        item.setAttribute('src', imgPath);
+        item.style.position = 'absolute';
+        const x = randomNumber(x1, x2);
+        const y = randomNumber(y1, y2);
+        item.style.left = `${x}px`;
+        item.style.right = `${y}px`;
+        field.appendChild(item);
+        console.log('lecture');
+    }
+}
+
+function randomNumber(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+initGame();
