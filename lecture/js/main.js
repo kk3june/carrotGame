ccb85df15f4febec1c32bc98ae5145ee2e329fb2
3jun <kk3june@gmail.com>
2021-03-13 12:12:02 +0900

    lecture 7.12 gameTimer start complete

--- lecture/js/main.js
+++ lecture/js/main.js
@@ -3,6 +3,7 @@
 const CARROT_SIZE = 80;
 const CARROT_COUNT = 5;
 const BUG_COUNT = 5;
+const GAME_DURATION_SEC = 5;
 
 const field = document.querySelector('.game__field');
 const fieldRect = field.getBoundingClientRect();
@@ -27,6 +28,7 @@ function startGame() {
     initGame();                 //게임이 시작되었을 때 이미지들이 생성되어야 하기 때문에
     showStopButton();           //게임이 시작되면 stop 버튼이 보여야한다.
     showTimerAndScore();        //게임이 시작되면 timer와 score가 보여야 한다.
+    startGameTimer();
 }
 
 function stopGame() {
@@ -36,7 +38,7 @@ function stopGame() {
 function showStopButton() {
     const  icon = gameBtn.querySelector('.fa-play');
     icon.classList.add('fa-stop');
-    icon.classList.remopve('fa-play');
+    icon.classList.remove('fa-play');
 }
 
 function showTimerAndScore() {
@@ -44,8 +46,27 @@ function showTimerAndScore() {
     gameScore.style.visibility = 'visible';
 }
 
+function startGameTimer() {
+    let remainingTimeSec =  GAME_DURATION_SEC;      //지정한 시간만 아래 setInterval 함수가 실행되어야 하기 때문에 
+    updateTimerText(remainingTimeSec);
+    timer = setInterval( ()=> {
+        if(remainingTimeSec <=0) {
+            clearInterval(timer);            //남은 시간이 없다면, 즉 0초라면 timer가 동작 되지 않아야 하기 때문에 clear
+            return;
+        }
+        updateTimerText(--remainingTimeSec);
+    }, 1000)
+}
+
+function updateTimerText(time) {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    gameTimer.innerText = `${minutes}:${seconds}`;
+}
+
 function initGame() {
-    console.log(fieldRect);
+    field.innerHTML = '';
+    gameScore.innerText = CARROT_COUNT;
     addItem('carrot', 5, '/lecture/img/carrot.png');
     addItem('bug', 5, '/lecture/img/bug.png');
 }
