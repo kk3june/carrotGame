a66b4c2bc7b6ea2489e742d3d5dac21a2d7a6425
3jun <kk3june@gmail.com>
2021-03-13 17:53:39 +0900

    lecture 7.14 stop game complete

--- lecture/js/main.js
+++ lecture/js/main.js
@@ -11,6 +11,10 @@ const gameBtn = document.querySelector('.game__button');
 const gameTimer = document.querySelector('.game__timer');
 const gameScore = document.querySelector('.game__score');
 
+const popUp = document.querySelector('.pop-up');
+const popUpText = document.querySelector('.pop-up__message');
+const popUpRefresh = document.querySelector('.pop-up__refresh');
+
 let started = false;
 let score = 0;
 let timer = undefined;
@@ -32,13 +36,19 @@ function startGame() {
 }
 
 function stopGame() {
-
+    stopGameTimer();        //타이머를 멈추기 위한 함수
+    hideGameButton();      // 플레이 버튼 사라짐
+    showPopUpWithText('REPLAY❓');      //팝업창 나타내기 위한 함수
 }
 
 function showStopButton() {
     const  icon = gameBtn.querySelector('.fa-play');
     icon.classList.add('fa-stop');
     icon.classList.remove('fa-play');
+}
+
+function hideGameButton() {
+    gameBtn.style.visibility = 'hidden';
 }
 
 function showTimerAndScore() {
@@ -58,10 +68,19 @@ function startGameTimer() {
     }, 1000)
 }
 
+function stopGameTimer() {
+    clearInterval(timer);
+}
+
 function updateTimerText(time) {
     const minutes = Math.floor(time / 60);
     const seconds = time % 60;
     gameTimer.innerText = `${minutes}:${seconds}`;
+}
+
+function showPopUpWithText(text) {
+    popUpText.innerText = text;
+    popUp.classList.remove('pop-up--hide');
 }
 
 function initGame() {
